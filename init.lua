49d65e74b2f6e442085f668550a35372ac4ff7f5
Nick Poorman <nickpoorman@gmail.com>
2024-07-03 15:33:16 -0600

    Update

--- init.lua
+++ init.lua
@@ -1,6 +1,9 @@
 return {
-	"yourusername/nvim-rubocop-textwidth",
+	"nickpoorman/nvim-rubocop-textwidth",
 	ft = "ruby", -- Load this plugin for Ruby files only
+	dependencies = {
+		{ "https://github.com/lubyk/yaml" }, -- Add the yaml library as a dependency
+	},
 	config = function()
 		require("rubocop_textwidth").setup()
 	end,
