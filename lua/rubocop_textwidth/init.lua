2c45d10f07f6d788f07584cec90f65597c32d085
Nick Poorman <nickpoorman@gmail.com>
2024-07-03 18:02:51 -0600

    More tries

--- lua/rubocop_textwidth/init.lua
+++ lua/rubocop_textwidth/init.lua
@@ -17,28 +17,28 @@ if not lyaml_ok then
 	return M
 end
 
--- Function to set textwidth from rubocop --show-cops
-local function set_textwidth_from_rubocop()
-	local cwd = vim.fn.getcwd()
-	local rubocop_path = cwd .. "/.rubocop.yml"
+function M.setup()
+	vim.notify("rubocop_textwidth.setup called", vim.log.levels.INFO)
+	-- Function to set textwidth from rubocop --show-cops
+	local function set_textwidth_from_rubocop()
+		local cwd = vim.fn.getcwd()
+		local rubocop_path = cwd .. "/.rubocop.yml"
 
-	if vim.fn.filereadable(rubocop_path) == 1 then
-		local handle = io.popen("bundle exec rubocop --show-cops")
-		local result = handle:read("*all")
-		handle:close()
+		if vim.fn.filereadable(rubocop_path) == 1 then
+			local handle = io.popen("bundle exec rubocop --show-cops")
+			local result = handle:read("*all")
+			handle:close()
 
-		if result then
-			local config = lyaml.load(result)
-			if config and config["Layout/LineLength"] and config["Layout/LineLength"].Max then
-				local max_line_length = config["Layout/LineLength"].Max
-				vim.opt.textwidth = max_line_length
+			if result then
+				local config = lyaml.load(result)
+				if config and config["Layout/LineLength"] and config["Layout/LineLength"].Max then
+					local max_line_length = config["Layout/LineLength"].Max
+					vim.opt.textwidth = max_line_length
+				end
 			end
 		end
 	end
-end
 
-function M.setup()
-	vim.notify("rubocop_textwidth.setup called", vim.log.levels.INFO)
 	-- Call the function when a Ruby file is opened
 	vim.api.nvim_create_autocmd("BufReadPost", {
 		pattern = "*.rb",
